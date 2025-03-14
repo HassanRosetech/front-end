347f580961b1d5f8a697b136ff9a426e36329cb7
Hassan Barbour <barbour.hassan@gmail.com>
2025-03-14 12:59:21 +0300

    Sony-Xperia L Series

---
+++ pages/Sony-Xperia/L-Series.vue
@@ -0,0 +1,42 @@
+<template>
+  <Head>
+    <Title>Sony Xperia - L Series</Title>
+  </Head>
+  <layout3 pageName="Search" parent="Search">
+    <PageSearchTopBar v-model="search" />
+    <PageSearchProductSlider :productsList="dataToShow" />
+  </layout3>
+</template>
+
+<script setup>
+import layout3 from "~/layout/layouts/layout3.vue";
+import { useProductStore } from "~~/store/SonyXperiaLSeries";
+
+let productList = computed(() =>
+  useProductStore().data.filter((item) => item.type === "Sony Xperia L Series")
+);
+let search = ref(""),
+  dataToShow = ref();
+dataToShow.value = productList.value;
+watch(
+  () => search,
+  () => {
+    if (search.value == "") {
+      dataToShow.value = productList.value;
+    } else {
+      dataToShow.value = productList.value.filter((item) => {
+        return item.name
+          .toLocaleLowerCase()
+          .includes(search.value.toLocaleLowerCase());
+      });
+    }
+  },
+  { deep: true }
+);
+</script>
+<style>
+.search-section .search-bar .input-group {
+  width: 50%;
+  margin: 26px auto 0px;
+}
+</style>
