79deb7a3a5d30c0c1709a1fec3cb5febfce94799
Hassan Barbour <barbour.hassan@gmail.com>
2025-03-14 10:21:36 +0300

    samsung galaxy M

---
+++ pages/samsung/Galaxy-M-Series.vue
@@ -0,0 +1,44 @@
+<template>
+  <Head>
+    <Title>Samsung - Galaxy M series</Title>
+  </Head>
+  <layout3 pageName="Search" parent="Search">
+    <PageSearchTopBar v-model="search" />
+    <PageSearchProductSlider :productsList="dataToShow" />
+  </layout3>
+</template>
+
+<script setup>
+import layout3 from "~/layout/layouts/layout3.vue";
+import { useProductStore } from "~~/store/samsungGalaxyMseries";
+
+let productList = computed(() =>
+  useProductStore().data.filter(
+    (item) => item.type === "Samsung Galaxy M Series"
+  )
+);
+let search = ref(""),
+  dataToShow = ref();
+dataToShow.value = productList.value;
+watch(
+  () => search,
+  () => {
+    if (search.value == "") {
+      dataToShow.value = productList.value;
+    } else {
+      dataToShow.value = productList.value.filter((item) => {
+        return item.name
+          .toLocaleLowerCase()
+          .includes(search.value.toLocaleLowerCase());
+      });
+    }
+  },
+  { deep: true }
+);
+</script>
+<style>
+.search-section .search-bar .input-group {
+  width: 50%;
+  margin: 26px auto 0px;
+}
+</style>
