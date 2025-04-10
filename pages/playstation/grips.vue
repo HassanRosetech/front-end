ea7ec24ea7cac068c0a71ae3b3e1621ecf5ece68
Hassan Barbour <barbour.hassan@gmail.com>
2025-04-10 17:04:00 +0300

    Gaming1

---
+++ pages/playstation/grips.vue
@@ -0,0 +1,42 @@
+<template>
+  <Head>
+    <Title>PlayStation - Grips</Title>
+  </Head>
+  <layout3 pageName="Search" parent="Search">
+    <PageSearchTopBar v-model="search" />
+    <PageSearchProductSlider :productsList="dataToShow" />
+  </layout3>
+</template>
+
+<script setup>
+import layout3 from "~/layout/layouts/layout3.vue";
+import { useProductStore } from "~~/store/grips";
+
+let productList = computed(() =>
+  useProductStore().data.filter((item) => item.type === "grips")
+);
+let search = ref(""),
+  dataToShow = ref();
+dataToShow.value = productList.value;
+watch(
+  () => search,
+  () => {
+    if (search.value == "") {
+      dataToShow.value = productList.value;
+    } else {
+      dataToShow.value = productList.value.filter((item) => {
+        return item.name
+          .toLocaleLowerCase()
+          .includes(search.value.toLocaleLowerCase());
+      });
+    }
+  },
+  { deep: true }
+);
+</script>
+<style>
+.search-section .search-bar .input-group {
+  width: 50%;
+  margin: 26px auto 0px;
+}
+</style>
