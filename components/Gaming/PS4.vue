e050efd621d699d4ac46a8c6a50938c19165ef5b
Hassan Barbour <barbour.hassan@gmail.com>
2025-05-07 20:38:02 +0300

    gming links

--- components/Gaming/PS4.vue
+++ components/Gaming/PS4.vue
@@ -7,12 +7,18 @@
   </div>
 
   <ul v-show="!collapsed['PS4']" class="subcategory1 tree-children">
-    <li><NuxtLink to="#">Kablar</NuxtLink></li>
     <li>
-      <NuxtLink to="#">Laddningsstationer</NuxtLink>
+      <NuxtLink to="/shop/shop_left_sidebar/PlayStation">Kablar</NuxtLink>
     </li>
     <li>
-      <NuxtLink v-show="!collapsed['Skydd och Grepp']" to="#"
+      <NuxtLink to="/shop/shop_left_sidebar/PlayStation"
+        >Laddningsstationer</NuxtLink
+      >
+    </li>
+    <li>
+      <NuxtLink
+        v-show="!collapsed['Skydd och Grepp']"
+        to="/shop/shop_left_sidebar/PlayStation"
         >Skydd och Grepp</NuxtLink
       >
       <div class="tree-item">
@@ -28,20 +34,32 @@
           class="subcategory1 tree-children"
         >
           <li>
-            <NuxtLink to="#">Spak grepp</NuxtLink>
+            <NuxtLink to="/shop/shop_left_sidebar/PlayStation"
+              >Spak grepp</NuxtLink
+            >
           </li>
           <li>
-            <NuxtLink to="#">Silikon hölje Kit</NuxtLink>
+            <NuxtLink to="/shop/shop_left_sidebar/PlayStation"
+              >Silikon hölje Kit</NuxtLink
+            >
           </li>
           <li>
-            <NuxtLink to="#">Handkontrollgrepp</NuxtLink>
+            <NuxtLink to="/shop/shop_left_sidebar/PlayStation"
+              >Handkontrollgrepp</NuxtLink
+            >
           </li>
-          <li><NuxtLink to="#">Hel set</NuxtLink></li>
+          <li>
+            <NuxtLink to="/shop/shop_left_sidebar/PlayStation"
+              >Hel set</NuxtLink
+            >
+          </li>
         </ul>
       </div>
     </li>
     <li>
-      <NuxtLink v-show="!collapsed['Väskor och fodral']" to="#"
+      <NuxtLink
+        v-show="!collapsed['Väskor och fodral']"
+        to="/shop/shop_left_sidebar/PlayStation"
         >Väskor och fodral</NuxtLink
       >
       <div class="tree-item">
@@ -60,13 +78,17 @@
           class="subcategory1 tree-children"
         >
           <li>
-            <NuxtLink to="#">Handkontroll</NuxtLink>
+            <NuxtLink to="/shop/shop_left_sidebar/PlayStation"
+              >Handkontroll</NuxtLink
+            >
           </li>
         </ul>
       </div>
     </li>
     <li>
-      <NuxtLink v-show="!collapsed['Franchise-produkter']" to="#"
+      <NuxtLink
+        v-show="!collapsed['Franchise-produkter']"
+        to="/shop/shop_left_sidebar/PlayStation"
         >Franchise-produkter</NuxtLink
       >
 
@@ -86,22 +108,34 @@
           class="subcategory1 tree-children"
         >
           <li>
-            <NuxtLink to="#">DC Batman</NuxtLink>
+            <NuxtLink to="/shop/shop_left_sidebar/PlayStation"
+              >DC Batman</NuxtLink
+            >
           </li>
           <li>
-            <NuxtLink to="#">Dragon Ball</NuxtLink>
+            <NuxtLink to="/shop/shop_left_sidebar/PlayStation"
+              >Dragon Ball</NuxtLink
+            >
           </li>
           <li>
-            <NuxtLink to="#">One Piece</NuxtLink>
+            <NuxtLink to="/shop/shop_left_sidebar/PlayStation"
+              >One Piece</NuxtLink
+            >
           </li>
           <li>
-            <NuxtLink to="#">Resident Evil</NuxtLink>
+            <NuxtLink to="/shop/shop_left_sidebar/PlayStation"
+              >Resident Evil</NuxtLink
+            >
           </li>
           <li>
-            <NuxtLink to="#">Tanooki</NuxtLink>
+            <NuxtLink to="/shop/shop_left_sidebar/PlayStation"
+              >Tanooki</NuxtLink
+            >
           </li>
           <li>
-            <NuxtLink to="#">Captain Tsubasa</NuxtLink>
+            <NuxtLink to="/shop/shop_left_sidebar/PlayStation"
+              >Captain Tsubasa</NuxtLink
+            >
           </li>
         </ul>
       </div>
